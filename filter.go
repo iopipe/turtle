5c3a561e0bda175e83d5d33dfaf2c171e775c7f0
Eric Windisch <eric@iopipe.com>
2015-12-09 14:11:35 -0500

    Add RequireJS; gofmt
    Signed-off-by: Eric Windisch <eric@iopipe.com>

--- filter.go
+++ filter.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-        "github.com/robertkrimen/otto"
+	"github.com/robertkrimen/otto"
 
-        //"errors"
+	//"errors"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 const FILTER_BASE string = "http://192.241.174.50/filters/"
+const REQUIREJS_URL string = "http://requirejs.org/docs/release/2.1.22/r.js"
 
 type filterTuple struct {
 	fromObjType string
@@ -17,20 +18,41 @@ type filterTuple struct {
 }
 
 func findFilter(fromObjType string, toObjType string) (func(input string) (string, error), error) {
-	path := FILTER_BASE + fromObjType + "/" + toObjType;
-        res, err := http.Get(path)
-        if err != nil {
-                log.Fatal(err)
-        }
-        body, err := ioutil.ReadAll(res.Body)
-        if err != nil {
-                log.Fatal(err)
-        }
-        res.Body.Close()
-        script := string(body[:])
+	var (
+		res  *http.Response
+		body []byte
+		err  error
+	)
+
+	res, err = http.Get(REQUIREJS_URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res.Body.Close()
+	rjs := string(body[:])
+
+	path := FILTER_BASE + fromObjType + "/" + toObjType
+	res, err = http.Get(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res.Body.Close()
+	script := string(body[:])
 
 	return func(input string) (string, error) {
 		vm := otto.New()
+
+		println("Adding RequireJS")
+		vm.Run(rjs)
+
 		vm.Set("input", input)
 		println("Executing script: " + script)
 		val, err := vm.Run(script)
@@ -41,4 +63,3 @@ func findFilter(fromObjType string, toObjType string) (func(input string) (strin
 	}, nil
 
 }
-
